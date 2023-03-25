61551e8c32dc1b0b4b7ddfe3e0a1548766d5b73b
kavinsaravanan <kavin.saravanan101@gmail.com>
2023-03-25 17:50:31 -0400

    changed user model and routes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const http = require('http').Server(app);
+const cors = require('cors');
 
 const router = require('./routes/index.js');
 
