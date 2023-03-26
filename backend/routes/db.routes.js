54ab120c275c5503dc3161062860846523cd812d
Arush Mehrotra <43364437+arush-mehrotra@users.noreply.github.com>
2023-03-25 21:21:01 -0400

    added db_credentials + connection

---
+++ backend/routes/db.routes.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql')
+const config = require('../config.json')
+const { Router } = require("express");
+const dbRouter = Router();
+
+const connection = mysql.createConnection({
+    host: config.rds_host,
+    user: config.rds_user,
+    password: config.rds_password,
+    port: config.rds_port,
+    database: config.rds_db
+  });
+  connection.connect((err) => err && console.log(err));
+
+dbRouter.get("/countryList", async (req, res) => {
+    connection.query(`
+    SELECT *
+    FROM Country
+    `, (err, data) => {
+      if (err || data.length === 0) {
+        console.log(err);
+        res.json([]);
+      } else {
+        res.json(data);
+      }
+    }); 
+});
+
+  module.exports = {
+    dbRouter
+  }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const { usersRouter } = require("./user.routes");
+const { dbRouter } = require("./db.routes");
 
 const router = Router();
 
 router.use('/user', usersRouter);
+router.use('/sql', dbRouter);
 
 module.exports = router;
