a486f70594fd6194a120ac1b17a377058688605f
kevinz730 <88609518+kevinz730@users.noreply.github.com>
2023-04-08 11:50:40 -0400

    add preliminary routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const { usersRouter } = require("./user.routes");
 const { dbRouter } = require("./db.routes");
+const { matchRouter } = require("./matches")
 
 const router = Router();
 
 router.use('/user', usersRouter);
 router.use('/sql', dbRouter);
+router.use('/matches', matchRouter)
 
 module.exports = router;
