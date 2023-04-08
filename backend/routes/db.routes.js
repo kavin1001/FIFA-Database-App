b978e0a5bcb6729f22b21cecfa516a07801f65cb
kavinsaravanan <kavin.saravanan101@gmail.com>
2023-04-08 15:10:28 -0400

    search works

--- backend/routes/db.routes.js
+++ backend/routes/db.routes.js
@@ -1,68 +1,53 @@
-const mysql = require('mysql')
-const config = require('../config.json')
+const mysql = require("mysql");
+const config = require("../config.json");
 const { Router } = require("express");
 const dbRouter = Router();
 
 const connection = mysql.createConnection({
-    host: config.rds_host,
-    user: config.rds_user,
-    password: config.rds_password,
-    port: config.rds_port,
-    database: config.rds_db
-  });
+  host: config.rds_host,
+  user: config.rds_user,
+  password: config.rds_password,
+  port: config.rds_port,
+  database: config.rds_db,
+});
 connection.connect((err) => err && console.log(err));
-
 
 // Test query to make sure connection to the database works
 
 dbRouter.get("/countryList", async (req, res) => {
-    connection.query(`
+  connection.query(
+    `
     SELECT *
     FROM Country
-    `, (err, data) => {
+    `,
+    (err, data) => {
       if (err || data.length === 0) {
         console.log(err);
         res.json([]);
       } else {
         res.json(data);
       }
-    }); 
+    }
+  );
 });
 
-<<<<<<< HEAD
-
-// Query to get all the players from the database
 dbRouter.get("/playerList", async (req, res) => {
-    connection.query(`
-    SELECT *
-    FROM Player
-    `, (err, data) => {
-      if (err || data.length === 0) {
-        console.log(err);
-        res.json([]);
-      } else {
-        res.json(data);
-      }
-    });
-});
-
-
-=======
-dbRouter.get("/playerList", async (req, res) => {
-  connection.query(`
+  connection.query(
+    `
   SELECT *
   FROM Player
-  `, (err, data) => {
-    if (err || data.length === 0) {
-      console.log(err);
-      res.json([]);
-    } else {
-      res.json(data);
+  `,
+    (err, data) => {
+      if (err || data.length === 0) {
+        console.log(err);
+        res.json([]);
+      } else {
+        res.json(data);
+      }
     }
-  }); 
+  );
 });
 
->>>>>>> 948015b42dc8848f6957b58376bdfac67b198826
-  module.exports = {
-    dbRouter
-  }
+module.exports = {
+  dbRouter,
+};
