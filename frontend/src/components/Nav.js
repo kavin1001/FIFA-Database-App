529c3b5d22ebd7ad16000bee782b0ed3246e9340
kavinsaravanan <kavin.saravanan101@gmail.com>
2023-04-26 17:18:52 -0400

    Added google sign-on

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -2,39 +2,66 @@ import { UserStore } from "../context/UserStore";
 import { useNavigate } from "react-router-dom";
 
 export default function Nav() {
-  const { userData, logout } = UserStore();
+  const { setUserData, userData, logout, thirdPartySignOn, setThirdParty } = UserStore();
   const navigate = useNavigate();
-  
-  const submitLogout = async() => {
-    await logout(userData.username);
-    navigate("/login");
 
-  }
+  const submitLogout = async () => {
+    console.log("logging out", thirdPartySignOn);
+    if (thirdPartySignOn) {
+      setThirdParty(false);
+      setUserData(null);
+      navigate("/login");
+    } else {
+      await logout(userData.username);
+      navigate("/login");
+    }
+  };
 
   return (
     <nav class="flex items-center justify-between flex-wrap bg-custom-purple p-6">
-    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-      <div class="text-sm lg:flex-grow">
-        <a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
-          Home
-        </a>
-        <a href="/player-search" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
-          Players
-        </a>
-        <a href="/team-search" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
-          Teams
-        </a>
-        <a href="/matches" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
-          Matches
-        </a>
-        <a href="/profile" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white">
-          Profile
-        </a>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow">
+          <a
+            href="/"
+            class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+          >
+            Home
+          </a>
+          <a
+            href="/player-search"
+            class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+          >
+            Players
+          </a>
+          <a
+            href="/team-search"
+            class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+          >
+            Teams
+          </a>
+          <a
+            href="/matches"
+            class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+          >
+            Matches
+          </a>
+          <a
+            href="/profile"
+            class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white"
+          >
+            Profile
+          </a>
+        </div>
+        <div>
+          <button
+            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-custom-purple hover:bg-white mt-4 lg:mt-0"
+            onClick={submitLogout}
+          >
+            {" "}
+            Logout
+          </button>
+        </div>
       </div>
-      <div>
-        <button class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-custom-purple hover:bg-white mt-4 lg:mt-0" onClick={submitLogout}> Logout</button>
-      </div>
-    </div>
-  </nav>
+    </nav>
   );
 }
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -3,8 +3,11 @@ import Nav from "../components/Nav";
 import { UserStore } from "../context/UserStore";
 
 function Profile() {
-    const { userData } = UserStore();
-    if (userData) {
+    const { userData, thirdPartySignOn } = UserStore();
+
+    const imageURL = userData.picture;
+
+    if (userData && !thirdPartySignOn) {
         return (
             <div>
             <Nav/>
@@ -49,9 +52,43 @@ function Profile() {
             </div>
             </div>
         );
+    } else if (thirdPartySignOn && imageURL != null) {
+        return (
+            <div>
+            <Nav/>
+                <div class="w-full max-w-lg mx-auto mt-12">
+                <p className="text-3xl text-center font-bold mb-6">Your Profile Page</p>
+                    <div class="mx-auto mb-6 h-16 w-16 rounded-full overflow-hidden">
+                        <img src={userData.picture} alt="Profile"/>
+                    </div>
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                                First Name
+                            </label>
+                            <p>{userData.given_name}</p>
+                        </div>
+                        <div class="w-full md:w-1/2 px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                                Last Name
+                            </label>
+                            <p>{userData.family_name}</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-username">
+                                Username
+                            </label>
+                            <p>{userData.email}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     } else {
         <div>
-            <h1 className="font-bold text-center">Not logged in</h1>
+            <h1 className="font-bold text-center">Loading</h1>
         </div>
     }
 }
