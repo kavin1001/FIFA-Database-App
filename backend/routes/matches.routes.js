b24fd621308d7aa8abb500baf5685d30599c3f87
kevinz730 <88609518+kevinz730@users.noreply.github.com>
2023-04-30 23:10:26 -0400

    remove unneeded route

--- backend/routes/matches.routes.js
+++ backend/routes/matches.routes.js
@@ -61,24 +61,6 @@ matchRouter.get("/matchList/:league", async (req, res) => {
   }); 
 });
 
-matchRouter.get("/seasonMatches/:season", async (req, res) => {
-  connection.query(`
-    select M.id, country_id, league_id, season, stage, date AS date_date,
-    home_team_api_id, away_team_api_id, home_team_goal, away_team_goal, T1.team_long_name as home_team_name, T2.team_long_name as away_team_name
-    from Matches M
-    join Team T1 on M.home_team_api_id = T1.team_api_id
-    join Team T2 on M.away_team_api_id = T2.team_api_id
-    where season = '${req.params.season}';
-  `, (err, data) => {
-    if (err || data.length === 0) {
-      console.log(err);
-      res.json([]);
-    } else {
-      res.json(data);
-    }
-  }); 
-});
-
 matchRouter.get("/dateMatches/topPlayers/:startDate", async (req, res) => {
   connection.query(`
   WITH Players AS (
